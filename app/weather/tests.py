4643c1092883533d4e236b7f55c6f697f181fbae
Genek91 <newtowngenek@gmail.com>
2024-07-21 22:27:29 +0300

    добавление setUp в тесты

--- app/weather/tests.py
+++ app/weather/tests.py
@@ -1,10 +1,18 @@
+from http import HTTPStatus
+
 import unittest
 from django.test import Client
 
 
 class WeatherTests(unittest.TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
     def test_get_index(self):
-        client = Client()
-        response = client.get('/')
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_form_display(self):
+        response = self.client.get('/')
+        self.assertIn('form', response.context)
